5c78896b98f34aab66dcb9f65cbcb19602e90b15
bharat <bhartbhammar3336@gmail.com>
2024-05-02 20:45:29 +0530

    SMTP service added

--- src/workers/email.worker.js
+++ src/workers/email.worker.js
@@ -4,8 +4,9 @@ import nodemailer from "nodemailer";
 async function sendEmail(data) {
     try {
         const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
+            // host: process.env.SMTP_HOST,
+            // port: process.env.SMTP_PORT,
+            service: "gmail",
             auth: {
                 user: process.env.SMTP_USERNAME,
                 pass: process.env.SMTP_PASSWORD,
